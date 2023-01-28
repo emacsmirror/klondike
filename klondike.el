886a84cbb9a9988f8625b9123c4d9fb94381c34b
Jean Libète <tomenzgg@mail.mayfirst.org>
2023-01-28 09:57:43 -0600

    Alright; let's get this started.

---
+++ klondike.el
@@ -0,0 +1,28 @@
+;;; klondike.el --- Klondike                   -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Jean Libète
+
+;; Author: Jean Libète <tomenzgg@mail.mayfirst.org>
+;; URL: https://codeberg.org/tomenzgg/Emacs-Klondike
+;; Package-Requires: ((emacs "28.1"))
+;; Version: 1.0
+;; Keywords: game, rpg
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Sterf
+
+;;; Code:
